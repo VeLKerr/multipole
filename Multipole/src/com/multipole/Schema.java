2c0321c3c5562b31517524ba650de949ae3d2281
k.veLKerr <k.velkerr@gmail.com>
2014-10-13 16:08:30 +0000

    Added generation algorithm.

--- Multipole/src/com/multipole/Schema.java
+++ Multipole/src/com/multipole/Schema.java
@@ -2,6 +2,7 @@
 package com.multipole;
 
 import com.multipole.vertex.Vertex;
+import com.multipole.vertex.VertexComparator;
 import com.multipole.vertex.VertexType;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,9 +11,12 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import org.apache.commons.collections4.map.MultiValueMap;
 
 /**
@@ -20,53 +24,96 @@ import org.apache.commons.collections4.map.MultiValueMap;
  * @author Ivchenko Oleg (Kirius VeLKerr)
  */
 public class Schema {
+    public static final int UNDEFINED = -1;
     public static final int VAR_CNT = 3;
     private final MultiValueMap<Integer, Integer> ajacencyMap;
-    private final List<Vertex> vertices;
+    private final Set<Vertex> vertices;
     
     public Schema(){
         this.ajacencyMap = new MultiValueMap<>();
-        this.vertices = new ArrayList<>();
+        this.vertices = new TreeSet<>(new VertexComparator());
     }
     
-    private int add1VarMulipole(){
+    private Vertex[] add1VarMulipole(int cnt){
         int initialSize = vertices.size();
-        vertices.add(new Vertex(vertices.size(), VertexType.INPUT));
-        vertices.add(new Vertex(vertices.size(), VertexType.NOT));
+        Vertex[] inputs = new Vertex[2];
+        Vertex input = new Vertex(vertices.size(), VertexType.INPUT, InitialMasives.getInitialValues(cnt));
+        inputs[0] = input;
+        vertices.add(input);
+        Vertex not = new Vertex(vertices.size(), VertexType.NOT, input);
+        inputs[1] = not;
+        vertices.add(not);
         ajacencyMap.put(initialSize, initialSize + 1);
-        return initialSize;
+        return inputs;
     }
     
-    private int[] generate1stLayer(){
-        int[] inputIndexes = new int[VAR_CNT];
+    private List<Vertex[]> generate1stLayer(){
+        List<Vertex[]> res = new ArrayList<>();
         for(int i=0; i<VAR_CNT; i++){
-            inputIndexes[i] = add1VarMulipole();
+            res.add(add1VarMulipole(i));
         }
-        return inputIndexes;
+        return res;
     }
     
-    private int addBinaryElement(int oneIndex, int secIndex, VertexType vt){
-        ajacencyMap.put(oneIndex, vertices.size());
-        ajacencyMap.put(secIndex, vertices.size());
-        vertices.add(new Vertex(vertices.size(), vt));
-        return vertices.size() - 1;
+    private Vertex addBinaryElement(Vertex vertex1, Vertex vertex2, VertexType vt){  
+        Vertex newVertex = new Vertex(vertices.size(), vt, vertex1, vertex2);
+        if(vertices.add(newVertex)){
+            ajacencyMap.put(vertex1.getIndex(), vertices.size());
+            ajacencyMap.put(vertex2.getIndex(), vertices.size());
+            return newVertex;
+        }
+        return null;
     }
     
-    public List<Integer> generate2stLayer(){ //Trere are many BUGs!!!
-        int[] inputs = generate1stLayer();
-        List<Integer> subOutputs = new ArrayList<>();
-        List<Integer> outputs = new ArrayList<>();
+    private List<Vertex> generate2stLayer(){
+        List<Vertex[]> inputs = generate1stLayer();
+        List<Vertex> subOutputs = new ArrayList<>();
+        List<Vertex> outputs = new ArrayList<>();
         for(int i=0; i<2; i++){ //добавляем 1-й ряд конъюнкций
             for(int j=0; j<2; j++){
-                subOutputs.add(addBinaryElement(inputs[0] + i, inputs[1] + j, VertexType.AND));
+                subOutputs.add(addBinaryElement(inputs.get(0)[i], inputs.get(1)[j], VertexType.AND));
             }
         }
-        for(int index: subOutputs){
+        for(Vertex vert: subOutputs){ //2-й ряд
             for(int j=0; j<2; j++){
-                outputs.add(addBinaryElement(index, inputs[2] + j, VertexType.AND));
+                outputs.add(addBinaryElement(vert, inputs.get(2)[j], VertexType.AND));
             }
         }
         return outputs;
+    }
+    
+    public void generate3stLayer(){
+        List<Vertex> inputs = generate2stLayer();
+        List<Vertex> output = new ArrayList<>();
+        for(int i=0; i<inputs.size(); i++){
+            for(int j=i+1; j<inputs.size(); j++){
+                output.add(addBinaryElement(inputs.get(i), inputs.get(j), VertexType.OR));
+            }
+        }
+        for(int i=0; i<1; i++){
+            output = generateNextLayer(inputs, output);
+        }        
+//        for(int i=0; i<ouptut2.size(); i++){
+//            for(int j=i+1; j<ouptut2.size(); j++){
+//                output4.add(addBinaryElement(i, j, VertexType.OR));
+//            }
+//        }
+    }
+    
+    private List<Vertex> generateNextLayer(List<Vertex> inputs, List<Vertex> outpPrevious){ //могут быть повторения!
+        List<Vertex> res = new ArrayList<>();
+        for(Vertex outP: outpPrevious){
+            for(Vertex inp: inputs){
+                if(outP.getParents().get(0).getIndex() != inp.getIndex() &&
+                   outP.getParents().get(1).getIndex() != inp.getIndex()){
+                    Vertex newV = addBinaryElement(outP, inp, VertexType.OR);
+                    if(newV != null){
+                        res.add(newV);
+                    }
+                }
+            }
+        }
+        return res;
     }
     
     @Override
@@ -80,7 +127,8 @@ public class Schema {
         for(Vertex v: vertices){
             if(v.isElement()){
                 sb.append(v.getIndex()).append(" ");
-                sb.append(v.getType().toString()).append("\n");
+                sb.append(v.getType().toString()).append(v.valuesToString())
+                        .append("\n");
             }
         }
         return sb.toString();
@@ -103,5 +151,16 @@ public class Schema {
             }
         }
         return res;
+    }
+    
+    private Vertex findBy(int index){
+        Iterator<Vertex> it = vertices.iterator();
+        while(it.hasNext()){
+            Vertex vert = it.next();
+            if(vert.getIndex() == index){
+                return vert;
+            }
+        }
+        return null;
     }
 }
